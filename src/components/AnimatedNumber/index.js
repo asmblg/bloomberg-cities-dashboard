3f6c2057a7598522f3a9c0bc4faec71b91ae3afe
ASMBLG <erik.woodworth@asmblg.io>
2022-12-15 14:27:30 -0600

    formatted animated number

--- src/components/AnimatedNumber/index.js
+++ src/components/AnimatedNumber/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import numeral from 'numeral';
 import { useSpring, animated } from 'react-spring';
 
-const AnimatedNumber = ({ value }) => {
+const AnimatedNumber = ({ value, type }) => {
   // Ensures only number or string values
   const n =
     typeof value === 'number' ? value : typeof value === 'string' && !isNaN(+value) ? +value : null;
@@ -14,8 +15,14 @@ const AnimatedNumber = ({ value }) => {
     delay: 0,
     config: { mass: 1, tension: 20, friction: 10 }
   });
+
+  const formatString = type === 'percent' || type === 'time'
+    ? '0.0'
+    : type === 'money'
+      ? '$0,0'
+        : '0,0'
   // If value is not passed in as a string that represents a number or number, nothing with be returned
-  return n ? <animated.div>{number.to(num => num.toFixed(decimalPlaces))}</animated.div> : null;
+  return n ? <animated.div>{number.to(num => `${numeral(num.toFixed(decimalPlaces)).format(formatString)}${type === 'percent' ? '%': ''}`)}</animated.div> : null;
 };
 
 export default AnimatedNumber;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,27 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import AnimatedNumber from '../AnimatedNumber';
+// import { LineChart, Line, ResponsiveContainer, YAxis } from 'recharts';
 import './style.css'
 
 
 const Card = ({ indicator, indicatorData, manifest }) => {
   const [mostRecentValue, setMostRecentValue] = useState();
   const [mostRecentYear, setMostRecentYear] = useState();
+  // const [lineChartData, setLineChartData] = useState();
   // console.log(indicatorData);
 
   useEffect(() => {
     const sortedKeys = Object.keys(indicatorData).sort((a, b) => b - a);
-    setMostRecentValue(indicatorData[sortedKeys[0]])
-    setMostRecentYear(sortedKeys[0])
+    // const indicatorDataArray= [...sortedKeys].sort((a,b) => a - b).map(key =>
+    //   ({
+    //     name: key,
+    //     value: indicatorData[key]
+    //   })
+    // )
+
+    setMostRecentValue(indicatorData[sortedKeys[0]]);
+    setMostRecentYear(sortedKeys[0]);
+    // setLineChartData(indicatorDataArray);
+    
   }, [indicatorData])
   return (
     <div className='card'>
       <div className='indicator-label'>
-        {manifest[indicator]}
+        {manifest[indicator].label}
       </div>
       <div className='indicator-year'>{mostRecentYear}</div>
       <div className='indicator-most-recent-value'>
-        <AnimatedNumber value={mostRecentValue} />
+        <AnimatedNumber value={mostRecentValue} type={manifest[indicator].type} />
       </div>
+      {/* <div className='indicator-sparkline'>
+        <ResponsiveContainer width='100%' height={50}>
+          <LineChart data={lineChartData || []} width={200} height={50}>
+            <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} />
+            <YAxis domain={['dataMin','dataMax']}/>
+          </LineChart>        
+        </ResponsiveContainer>
+
+
+      </div> */}
     </div>
   )
 };
