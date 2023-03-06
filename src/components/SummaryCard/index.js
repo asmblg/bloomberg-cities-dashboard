3103c1041437d80c22af6c6c35cab55286dd5ef4
John Herman <hermanjohn2@gmail.com>
2023-03-06 18:39:33 -0500

    Initialized SummaryCard. Updated some styling in SummaryCard parent components.

--- src/components/SummaryCard/index.js
+++ src/components/SummaryCard/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import upwardTrendIcon from './images/upward_trend_icon.png';
+import downwardTrendIcon from './images/downward_trend_icon.png';
+import './style.css';
+
 const SummaryCard = ({ config }) => {
+  const data = '';
+  const chartData = [];
+  const trendDirection = 'upward';
+  const trendChange = 'XX.X'; // add symbol
+  console.log(chartData);
+
   return (
-    <div>
-      <h1>{config.label}</h1>
+    <div className='summary-card'>
+      <div className='summary-header' role='heading'>
+        {config.label.toUpperCase()}
+      </div>
+      <div className='summary-data-wrapper'>
+        <div className='summary-data  standard-bold'>
+          <div className='summary-value'>{data || '-'}</div>
+          <div className='summary-units'>{config.units}</div>
+        </div>
+        <div className='summary-chart'></div>
+      </div>
+      {trendDirection === 'upward' || trendDirection === 'downward' ? (
+        <div className={trendDirection === 'upward' ? 'upward-trend' : 'downward-trend'}>
+          <img src={trendDirection === 'upward' ? upwardTrendIcon : downwardTrendIcon} />
+          <p>{`${trendDirection === 'upward' ? '+' : '-'} ${trendChange}`}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
