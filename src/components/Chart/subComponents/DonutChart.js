a9aa6366c00e47251084cb1a7ee77699ca8447e8
John Herman <hermanjohn2@gmail.com>
2023-03-07 13:32:15 -0500

    Initialized DonutChart.

--- src/components/Chart/subComponents/DonutChart.js
+++ src/components/Chart/subComponents/DonutChart.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ResponsiveContainer, PieChart, Pie, Cell, Label as PieLabel } from 'recharts';
 
-const DonutChart = ({ config }) => {
-  console.log(config);
-  return (
-    <div>
-      <h1>D0nut</h1>
-    </div>
-  );
+const DonutChart = ({ config, value }) => {
+  const { label, color, accentColor } = config;
+  const dataArray = [];
+  console.log(accentColor);
+
+  return value ? (
+    <ResponsiveContainer height={100} width={'100%'}>
+      <PieChart title={`${label}: ${value}`}>
+        <Pie
+          data={dataArray}
+          dataKey={'value'}
+          cx={'50%'}
+          cy={'50%'}
+          outerRadius={40}
+          innerRadius={25}
+          startAngle={-270}
+          endAngle={90}
+          fill={color || 'black'}
+        >
+          <PieLabel opacity={0.5} position={'center'} value={label} />
+          {dataArray.map(({ fillColor, value }, i) => (
+            <Cell key={`donut-chart-cell-${value}-${i}`} fill={fillColor} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  ) : null; // Spinner Here?
 };
 
 DonutChart.propTypes = {
-  config: PropTypes.object
+  config: PropTypes.object,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
 export default DonutChart;
