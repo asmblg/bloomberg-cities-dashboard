8f35af6d1717e38fca8e42138dc56851fdda782b
John Herman <hermanjohn2@gmail.com>
2023-03-07 13:37:23 -0500

    Commented out sample data on Chart subcomponents.

--- src/components/Chart/subComponents/ColumnChart.js
+++ src/components/Chart/subComponents/ColumnChart.js
@@ -4,29 +4,29 @@ import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from 'recharts';
 
 const ColumnChart = ({ config }) => {
   const { xaxis, color, accentColor } = config;
-  const testData = [
-    {
-      [xaxis]: 'Q1-22',
-      value: 200
-    },
-    {
-      [xaxis]: 'Q2-22',
-      value: 225
-    },
-    {
-      [xaxis]: 'Q3-22',
-      value: 300
-    },
-    {
-      [xaxis]: 'Q4-22',
-      value: 275
-    }
+  const dataArray = [
+    // {
+    //   [xaxis]: 'Q1-22',
+    //   value: 200
+    // },
+    // {
+    //   [xaxis]: 'Q2-22',
+    //   value: 225
+    // },
+    // {
+    //   [xaxis]: 'Q3-22',
+    //   value: 300
+    // },
+    // {
+    //   [xaxis]: 'Q4-22',
+    //   value: 275
+    // }
   ];
 
-  return (
+  return dataArray && dataArray[0] ? (
     <ResponsiveContainer height={100} width={'100%'}>
       <BarChart
-        data={testData}
+        data={dataArray}
         barGap={0}
         barCategoryGap={0}
         margin={{ top: 30, right: 0, bottom: -10, left: -29 }}
@@ -41,7 +41,7 @@ const ColumnChart = ({ config }) => {
         />
       </BarChart>
     </ResponsiveContainer>
-  );
+  ) : null; // Spinner?
 };
 
 ColumnChart.propTypes = {
--- src/components/Chart/subComponents/LineChart.js
+++ src/components/Chart/subComponents/LineChart.js
@@ -4,33 +4,33 @@ import { LineChart as LChart, XAxis, YAxis, Line, ResponsiveContainer } from 're
 
 const LineChart = ({ config }) => {
   const { xaxis, color } = config;
-  const testData = [
-    {
-      [xaxis]: 'Q1-22',
-      value: 125
-    },
-    {
-      [xaxis]: 'Q2-22',
-      value: 275
-    },
-    {
-      [xaxis]: 'Q3-22',
-      value: 200
-    },
-    {
-      [xaxis]: 'Q4-22',
-      value: 300
-    }
+  const dataArray = [
+    // {
+    //   [xaxis]: 'Q1-22',
+    //   value: 125
+    // },
+    // {
+    //   [xaxis]: 'Q2-22',
+    //   value: 275
+    // },
+    // {
+    //   [xaxis]: 'Q3-22',
+    //   value: 200
+    // },
+    // {
+    //   [xaxis]: 'Q4-22',
+    //   value: 300
+    // }
   ];
-  return (
+  return dataArray && dataArray[0] ? (
     <ResponsiveContainer height={100} width={'100%'}>
-      <LChart data={testData} margin={{ top: 30, right: 0, bottom: -10, left: -29 }}>
+      <LChart data={dataArray} margin={{ top: 30, right: 0, bottom: -10, left: -29 }}>
         <XAxis dataKey={xaxis} tickCount={1} />
         <YAxis axisLine={false} tickCount={1} />
         <Line dataKey={'value'} dot={false} stroke={color || 'black'} strokeWidth={4} />
       </LChart>
     </ResponsiveContainer>
-  );
+  ) : null; // Spinner/Loader?
 };
 
 LineChart.propTypes = {
