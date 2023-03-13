b7fc8d70611f0903bce5e557430ac4a2bba11d19
John Herman <hermanjohn2@gmail.com>
2023-03-13 14:58:19 -0400

    Initialized community profiles population bar charts. Pushing up current changes.

--- src/components/Chart/subComponents/HorizontalBarChart.js
+++ src/components/Chart/subComponents/HorizontalBarChart.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BarChart, Bar, ResponsiveContainer, XAxis, LabelList } from 'recharts';
+import { BarChart, Bar, ResponsiveContainer, XAxis, LabelList, YAxis } from 'recharts';
 
-const HorizontalBarChart = ({ color, height, width, dataArray }) => {
+const HorizontalBarChart = ({ color, height, width, dataArray, margin }) => {
   // const data = dataArray ? dataArray : [{ name: label, value: '65' }];
   // console.log(dataArray.length, color, label);
   return (
     <ResponsiveContainer height={height} width={width}>
-      <BarChart layout='vertical' data={dataArray}>
+      <BarChart layout='vertical' data={dataArray} margin={margin}>
         <XAxis type='number' hide />
-        <Bar dataKey={'value'} fill={color}>
-          <LabelList position={'right'} dataKey='value' />
+        <YAxis
+          type='category'
+          dataKey={'name'}
+          tickCount={dataArray.length}
+          axisLine={false}
+          tickLine={false}
+          // minTickGap={20}
+          padding={{ bottom: 20 }}
+        />
+        {/* <Label position={'left'} value={'test'} /> */}
+        <Bar dataKey={'value'} fill={color} barSize={20}>
+          <LabelList position={'right'} dataKey={'value'} />
         </Bar>
       </BarChart>
     </ResponsiveContainer>
@@ -22,7 +32,8 @@ HorizontalBarChart.propTypes = {
   color: PropTypes.string,
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  dataArray: PropTypes.array
+  dataArray: PropTypes.array,
+  margin: PropTypes.object
 };
 
 export default HorizontalBarChart;
--- src/components/DetailCard/index.js
+++ src/components/DetailCard/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Chart from '../Chart';
-import LastUpdate from '../LastUpdate';
+import CommunityProfile from './subComponents/CommunityProfile';
 
 import './style.css';
 
 const DetailCard = ({ config, sectionKey }) => {
   const { label } = config;
-
-  const loremIpsum =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl purus in mollis nunc sed id semper. Sodales ut eu sem integer vitae.';
 
   return (
     <div className='detail-card-wrapper'>
@@ -19,36 +15,7 @@ const DetailCard = ({ config, sectionKey }) => {
           <div className='detail-title bold-font'>{label.toUpperCase()}</div>
         </div>
       ) : (
-        <>
-          <div className='community-profile-section'>
-            <div className='detail-title'>
-              <div className='bold-font'>{label.toUpperCase()}</div>
-              <div className='thin-font'>{'Q4 2022 Update'}</div>
-              <LastUpdate date={'01/25/2023 11:55 AM'} />
-            </div>
-            <div className='population-data-wrapper'>
-              <div className='total-pop-title'>
-                <div>{'Total Population'}</div>
-                <div>{'999,999'}</div>
-              </div>
-              <div className='demographics-charts'>
-                <Chart
-                  config={{type: 'horizontal-bar', label: 'test', color: 'orange' }}
-                  data={[{ name: 'ind1', value: '26' }, { name: 'ind2', value: '45' }, { name: 'ind3', value: '13' }, { name: 'ind4', value: '67' }]}
-                  height={100}
-                  width={'100%'}
-                  // margin={{ top: 30, right: 0, bottom: -10, left: -29 }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className='community-profile-section'>
-            <div>{loremIpsum}</div>
-          </div>
-          <div className='community-profile-section'>
-            <div>{'MAP INDICATOR TITLE'}</div>
-          </div>
-        </>
+        <CommunityProfile label={label} />
       )}
     </div>
   );
