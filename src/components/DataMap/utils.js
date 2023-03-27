20382fad59876d89b6ffea216e9a63f379c8bb1d
John Herman <hermanjohn2@gmail.com>
2023-03-27 14:39:13 -0400

    Fixed issue with map sizing and containers being cut off on shorter desktop views.

--- src/components/DataMap/utils.js
+++ src/components/DataMap/utils.js
@@ -8,23 +8,19 @@ const handleBinning = ({ geoJSON, colors, indicator, numOfBins }) => {
   const valueArray = geoJSON.features
     .map(feature => parseFloat(feature.properties[indicator]))
     .sort((a, b) => a - b);
-  // .filter(val => val !== 0); // This allows 4 bins on pop_pacific_islander where the first 315 values are 0.
-  // console.log(valueArray, colors[0]);
+
   for (let i = 0; i < numOfBins; i++) {
     let p = null;
     const m = i + 1;
     if (i !== numOfBins - 1) {
       p = percentile(pRange * m, valueArray);
-    } else p = percentile(100, valueArray); // Using percentile(100, valueArray) cause issues with pop_asian and other cases
+    } else p = percentile(100, valueArray);
 
-    // Only adds a bin if the percentile is different from previous. Handles case where
-    if (!binArray.filter(({ percentile }) => p === percentile)[0]) {
-      binArray.push({
-        label: '',
-        percentile: p,
-        color: colors[i]
-      });
-    }
+    binArray.push({
+      label: '',
+      percentile: p,
+      color: colors[i]
+    });
   }
   const arrayWithLabels = [...binArray].map((obj, i) => {
     const prevValue = i !== 0 ? binArray[i - 1].percentile : 0;
