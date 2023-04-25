d046a809f2552d65a4641d83e01983c2bd7416b6
John Herman <hermanjohn2@gmail.com>
2023-04-25 11:33:57 -0400

    Made changes to Share/Print icons

--- src/components/ShareAndPrintIcons/index.js
+++ src/components/ShareAndPrintIcons/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import shareIcon from './images/share_icon.png';
 import printIcon from './images/print_icon.png';
 import './style.css';
 
 const ShareAndPrintIcons = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+
+
   return (
     <div className='share-print-icons'>
-      <img src={shareIcon} onClick={() => console.log('share!')} />
-      <img src={printIcon} onClick={() => console.log('print!')} />
+      {isCopied
+        ? <div>Link Copied!</div>
+        : null}
+      <img src={shareIcon} onClick={() => {
+        navigator.clipboard.writeText(window.location.href).then(() => {
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 1000);
+        });
+      }} />
+      <img src={printIcon} onClick={() => window.print()} />
     </div>
   );
 };
