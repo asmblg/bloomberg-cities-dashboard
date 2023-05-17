9131df104388fea6a1ea921dab4a249c35716687
John Herman <hermanjohn2@gmail.com>
2023-05-17 16:52:31 -0400

    Added SafeGraph partner logo to footer.

--- src/components/Footer/config.js
+++ src/components/Footer/config.js
@@ -4,11 +4,18 @@ import blockLogo from './logos/block.png';
 import deweyLogo from './logos/dewey.png';
 import jllLogo from './logos/jll.png';
 import dealroomLogo from './logos/dealroom.png';
+import SafeGraphLogo from './logos/SafeGraph.png';
 
 const footerConfig = {
   mainLogo: bloombergLogo,
   copyright: '2023 Bloomberg Associates. All Rights Reserved.',
   partners: [
+    {
+      key: 'dealroom',
+      name: 'Dealroom.co',
+      url: ' https://dealroom.co/',
+      logo: dealroomLogo
+    },
     {
       key: 'alignable',
       name: 'Alignable',
@@ -28,16 +35,16 @@ const footerConfig = {
       logo: deweyLogo
     },
     {
+      key: 'safeGraph',
+      name: 'SafeGraph',
+      url: 'https://www.safegraph.com/',
+      logo: SafeGraphLogo
+    },
+    {
       key: 'block',
       name: 'Block',
       url: 'https://block.xyz/',
       logo: blockLogo
-    },
-    {
-      key: 'dealroom',
-      name: 'Dealroom.co',
-      url: ' https://dealroom.co/',
-      logo: dealroomLogo
     }
   ]
 };
