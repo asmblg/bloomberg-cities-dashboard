6ca8256ecdc017c2bf68a414c938be9efd8827c1
ASMBLG <erik.woodworth@asmblg.io>
2023-09-07 17:00:12 -0500

    disabled dev settings

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
-import config from '../dev/configs.json';
+// import config from '../dev/configs.json';
 
 const baseURL = 'https://bloomberg-cities-api.herokuapp.com';
 const devURL = 'http://localhost:3001';
-const dev = true;
-const localConfig = false; 
+const dev = false;
+// const localConfig = false; 
 
 const getConfig = async (projectCity) => {
-  if (!localConfig) {
-    const res =  await axios.get(`${!dev ? baseURL : devURL}/config`, {
-      params: {
-        project: projectCity
-      }
-    });
-
-    return res?.data?.[0];
-  } else {
-    // LOCAL CONFIG FOR DEV
-    const res = config;
-    
-    if (res.length > 1) {
-      const obj = res.find(
-        ({ project }) => project.toLowerCase() === projectCity.toLowerCase()
-      );
-      return obj || res[0];
+  // if (!localConfig) {
+  const res =  await axios.get(`${!dev ? baseURL : devURL}/config`, {
+    params: {
+      project: projectCity
     }
-  }
+  });
+
+  return res?.data?.[0];
+  // } else {
+  //   // LOCAL CONFIG FOR DEV
+  //   const res = config;
+    
+  //   if (res.length > 1) {
+  //     const obj = res.find(
+  //       ({ project }) => project.toLowerCase() === projectCity.toLowerCase()
+  //     );
+  //     return obj || res[0];
+  //   }
+  // }
 };
 
 const getData = (project, select) =>
