11abca6c945cf7d17485637c20a8eae778eceab0
ASMBLG <erik.woodworth@asmblg.io>
2025-02-25 14:50:53 -0600

    fix: resolve issues with data rendering in IndicatorMap component

--- src/components/IndicatorMap/index.jsx
+++ src/components/IndicatorMap/index.jsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { MapContainer, TileLayer, GeoJSON, Tooltip } from 'react-leaflet';
 // import CustomTooltip from '../CustomTooltip';
+import { TailSpin } from 'react-loader-spinner';
+
 import IndicatorDropdown from '../IndicatorDropdown';
 import Legend from './subComponents/Legend';
+import { getGeoJSON } from '../../utils/API';
 
 import { handleBinning, handleGeoJSON, handleNoGeoJsonProp } from './utils';
 // import { getGeoJSON } from '../../utils/API';
 import formatValue from '../../utils/formatValue';
 
 import './style.css';
+import { set } from 'mongoose';
 
 const IndicatorMap = ({ config, geoJSON, project, getter }) => {
   const [bins, setBins] = useState(null);
   const [mapGeoJSON, setMapGeoJSON] = useState(null);
+  const [refGeoJSON, setRefGeoJSON] = useState([]);
   const [selectedIndicator, setSelectedIndicator] = useState(null);
   const [hoveredFeature, setHoveredFeature] = useState();  
 
@@ -32,6 +37,25 @@ const IndicatorMap = ({ config, geoJSON, project, getter }) => {
   if ( typeof varKey !== 'string') {
     varKey = selectedIndicator?.key || defaultSelection?.key;
   }
+
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      const refGeoJSONArray = [];
+      if (config?.refLayers?.[0]) {
+        for (const {geoType} of config.refLayers) {
+          getGeoJSON(project, geoType)
+          .then(({ data }) => {
+            const refGeoJSON = data[0];
+            refGeoJSONArray.push(refGeoJSON);
+            if (refGeoJSONArray.length === config.refLayers.length) {
+              console.log(refGeoJSONArray);
+              setRefGeoJSON(refGeoJSONArray);
+              resolve(refGeoJSONArray);
+            }
+        });
+      }}
+    })
+  }, [config?.refLayers]);
 
   useEffect(() => {
 
@@ -81,12 +105,13 @@ const IndicatorMap = ({ config, geoJSON, project, getter }) => {
           aggregator: selectedIndicator?.aggregator,
           range: config?.range,
           numOfBins,
-          manualBreaks: defaultSelection?.manualBreaks || selectedIndicator?.manualBreaks
+          manualBreaks: config?.manualBreaks || defaultSelection?.manualBreaks || selectedIndicator?.manualBreaks
         })
       );
       // console.log(bins);
     }
-  }, [
+  }, 
+  [
     selectedIndicator, 
     mapGeoJSON, 
     colors,
@@ -95,7 +120,7 @@ const IndicatorMap = ({ config, geoJSON, project, getter }) => {
 
   // console.log(mapGeoJSON);
   
-  return mapGeoJSON ? (
+  return (//mapGeoJSON ? (
     <div className='indicator-map-wrapper'>
       {!config.externalDropdown && (
         <>
@@ -107,111 +132,168 @@ const IndicatorMap = ({ config, geoJSON, project, getter }) => {
           />
         </>
       )}
-
+{
+      // mapGeoJSON 
+      // ? 
       <div className='indicator-map'>
-        <MapContainer
-          key={`indicator-map-${varKey || 'no-data'}`}
-          center={config.center}
-          zoom={config.zoom}
-          zoomControl={true}
-          zoomSnap={.25}
-          zoomDelta={.25}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/">Esri: World Light Gray Base Map</a>'
-            url='https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
-          />
-
-          <GeoJSON
-            eventHandlers={{
-              mouseover: e => {
-                const value = e.propagatedFrom?.feature?.properties?.[varKey];
-                const indicator = selectedIndicator?.label || defaultSelection?.label;
-                const geo = e.propagatedFrom?.feature?.properties?.[config.nameProperty?.key ? config.nameProperty?.key : 'Name'] || '';
-                const units = selectedIndicator?.units || defaultSelection?.units;
-                setHoveredFeature({value, indicator, geo, units});
-              }            
-            }}
-            key={`data-layer-${varKey || 'no-data' }`}
-            data={mapGeoJSON}
-            filter={feature => {
-              const noIndicator = !selectedIndicator && !defaultSelection ;
-              if (noIndicator) {
-                return true;
-              }
-              let value = feature.properties[varKey];
-              if (selectedIndicator?.aggregator === 'current') {
-                const aggregatorKey = Object.keys(value).sort(dateKey => {
-                  const year = dateKey.split('-')[0];
-                  const quater = dateKey.split('-')[1]?.replace('Q', '');
-                  return Number(year) + Number(quater);
-                })?.[0]
-                value = value[aggregatorKey];
-              }
-        
-              if (selectedIndicator?.dataPath) {
-                selectedIndicator?.dataPath.split('.').forEach(path => {
-                  value = value?.[path] || null;
-                });
-              }
-              if (!isNaN(Number(value)) && Number(value) > 0) {
-                return true;
-              } else {
-                return false;
-              }
-            }}
-            style={feature => {
-              let value = feature.properties[varKey];
-              if (selectedIndicator?.aggregator === 'current') {
-                const aggregatorKey = Object.keys(value).sort(dateKey => {
-                  const year = dateKey.split('-')[0];
-                  const quater = dateKey.split('-')[1]?.replace('Q', '');
-                  return Number(year) + Number(quater);
-                })?.[0]
-                value = value[aggregatorKey];
-              }
-        
-              if (selectedIndicator?.dataPath) {
-                selectedIndicator?.dataPath.split('.').forEach(path => {
-                  value = value?.[path] || null;
-                });
-              }
-
-              const color = bins?.filter(({ percentile }) => 
-                  value <= percentile
-                ).map(({ color }) => color)[0] || 'transparent';
-              // console.log(color);
-              return {
-                fillColor: color,
-                color: config?.strokeColor || color,
-                weight: config?.weight || 1,
-                // opacity: config?.opacity || 1,
-                fillOpacity: config?.opacity || 1
-              };
-            }}
+          <MapContainer
+            key={`indicator-map-${varKey ? 'data' : 'no-data'}`}
+            center={config.center}
+            zoom={config.zoom}
+            zoomControl={true}
+            zoomSnap={.25}
+            zoomDelta={.25}
           >
-          {config?.nameProperty?.prefix &&       
-            (<Tooltip>
-              <div className='indicator-map-tooltip'>
-                <p>{config?.nameProperty?.prefix || ''} {hoveredFeature?.geo}</p>
-                <strong>{formatValue(hoveredFeature?.value, hoveredFeature?.units)}</strong>
-              </div>
-            </Tooltip>)}
-          </GeoJSON>
-          {bins && !config.noLegend && (
-            <Legend
-              indicator={selectedIndicator || defaultSelection}
-              bins={bins}
-              strokeColor={config.strokeColor || 'black'}
+            <TileLayer
+              attribution='&copy; <a href="https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/">Esri: World Light Gray Base Map</a>'
+              url='https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
             />
-          )}
 
-        </MapContainer>
-      </div>
+            {
+                    mapGeoJSON 
+                    ? <GeoJSON
+                        eventHandlers={{
+                          mouseover: e => {
+                            if ( !config?.refGeoJSON ) {
+                              const value = e.propagatedFrom?.feature?.properties?.[varKey];
+                              const indicator = selectedIndicator?.label || defaultSelection?.label;
+                              const geo = e.propagatedFrom?.feature?.properties?.[config.nameProperty?.key ? config.nameProperty?.key : 'Name'] || '';
+                              const units = selectedIndicator?.units || defaultSelection?.units;
+                              setHoveredFeature({value, indicator, geo, units});
+                            }
+                          },
+                          mouseout: e => {
+                            setHoveredFeature(null);
+                          }            
+                        }}
+                        key={`data-layer-${varKey || 'no-data' }`}
+                        data={mapGeoJSON}
+                        filter={feature => {
+                          const noIndicator = !selectedIndicator && !defaultSelection ;
+                          if (noIndicator) {
+                            return true;
+                          }
+                          let value = feature.properties[varKey];
+                          if (selectedIndicator?.aggregator === 'current') {
+                            const aggregatorKey = Object.keys(value).sort(dateKey => {
+                              const year = dateKey.split('-')[0];
+                              const quater = dateKey.split('-')[1]?.replace('Q', '');
+                              return Number(year) + Number(quater);
+                            })?.[0]
+                            value = value[aggregatorKey];
+                          }
+                    
+                          if (selectedIndicator?.dataPath) {
+                            selectedIndicator?.dataPath.split('.').forEach(path => {
+                              value = value?.[path] || null;
+                            });
+                          }
+                          if (!isNaN(Number(value)) && Number(value) > 0) {
+                            return true;
+                          } else {
+                            return false;
+                          }
+                        }}
+                        style={feature => {
+                          let value = feature.properties[varKey];
+                          if (selectedIndicator?.aggregator === 'current') {
+                            const aggregatorKey = Object.keys(value).sort(dateKey => {
+                              const year = dateKey.split('-')[0];
+                              const quater = dateKey.split('-')[1]?.replace('Q', '');
+                              return Number(year) + Number(quater);
+                            })?.[0]
+                            value = value[aggregatorKey];
+                          }
+                    
+                          if (selectedIndicator?.dataPath) {
+                            selectedIndicator?.dataPath.split('.').forEach(path => {
+                              value = value?.[path] || null;
+                            });
+                          }
+
+                          const color = bins?.filter(({ percentile }) => 
+                              value <= percentile
+                            ).map(({ color }) => color)[0] || 'transparent';
+                          // console.log(color);
+                          return {
+                            fillColor: color,
+                            color: config?.strokeColor || color,
+                            weight: config?.weight || 1,
+                            // opacity: config?.opacity || 1,
+                            fillOpacity: config?.opacity || 1
+                          };
+                        }}
+                      >
+                      {!config?.refGeoJSON && hoveredFeature?.value &&     
+                        (<Tooltip>
+                          <div className='indicator-map-tooltip'>
+                            <p>{config?.nameProperty?.prefix || ''} {hoveredFeature?.geo}</p>
+                            <strong>{formatValue(hoveredFeature?.value, hoveredFeature?.units)}</strong>
+                          </div>
+                        </Tooltip>)}
+                      </GeoJSON>
+                    : null
+                    } 
+
+            {refGeoJSON.map((refGeoJSON, i) => (
+              <GeoJSON
+                key={`ref-layer-${i}`}
+                data={refGeoJSON}
+                eventHandlers={{
+                  mouseover: e => {
+                    const labelField = config?.refLayers?.[i]?.labelField || 'Name';
+                    const value = e.propagatedFrom?.feature?.properties?.[labelField];
+                    // const indicator = selectedIndicator?.label || defaultSelection?.label;
+                    // const geo = e.propagatedFrom?.feature?.properties?.[config.nameProperty?.key ? config.nameProperty?.key : 'Name'] || '';
+                    // const units = selectedIndicator?.units || defaultSelection?.units;
+                    setHoveredFeature({value});
+                  },
+                  mouseout: e => {
+                    setHoveredFeature(null);
+                  }            
+                }}
+                style={{
+                  fillColor: 'transparent',
+                  color: 'white',
+                  weight: 1,
+                  fillOpacity: 0
+                }}
+                // style={config?.refStyles?.[i]}
+              >
+              {config?.refLayers[0]?.labelField && hoveredFeature?.value &&     
+              (<Tooltip>
+                <div>
+                  <h3>{hoveredFeature?.value}</h3>
+                </div>
+              </Tooltip>)}
+              </GeoJSON>
+            ))}
+            {bins && !config.noLegend && (
+              <Legend
+                indicator={selectedIndicator || defaultSelection}
+                bins={bins}
+                strokeColor={config.strokeColor || 'black'}
+              />
+            )}
+
+          </MapContainer>
+        </div>
+      // : null
+        // <TailSpin
+        //   color={'#006aaf'}
+        //   width={200}
+        //   height={200}
+        // />    // <div className='indicator-map-wrapper'>Loading...</div>
+  }
     </div>
-  ) : (
-    <div className='indicator-map-wrapper'>Loading...</div>
-  );
+  // ) : (
+  //       <TailSpin
+  //         color={'#006aaf'}
+  //         width={200}
+  //         height={200}
+  //       />    // <div className='indicator-map-wrapper'>Loading...</div>
+  // );
+  )
 };
 
 IndicatorMap.propTypes = {
