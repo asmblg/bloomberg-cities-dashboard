2ab12805b9ba3a2dc3886b1c2319d7b90ca56ab8
ASMBLG <erik.woodworth@asmblg.io>
2025-02-24 18:33:20 -0600

    chore: comment out local config code in API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import config from '../../dev/dev-configs.json';
+// import config from '../../dev/dev-configs.json';
 
 // const dev = true;
 // const prodURL = 'https://bloomberg-cities-api.herokuapp.com';
 // const devURL = 'https://bloomberg-cities-api-eu-4357e11f365b.herokuapp.com'; //'http://localhost:3001'; // ; 'http://192.168.0.111:3001'; // 
 // const baseURL = !dev ? prodURL : devURL;
-const localConfig = true;
+const localConfig = false;
 // const baseURL = import.meta.env.VITE_API_URL;
 
 
@@ -21,19 +21,19 @@ const getConfig = async projectCity => {
     return res?.data?.[0];
   } else {
     // LOCAL CONFIG FOR DEV
-    return await new Promise((resolve, reject) => {
-      const res = config;
+    // return await new Promise((resolve, reject) => {
+    //   const res = config;
   
-      if (res.length >= 1) {
-        const obj = res.find(
-          ({ project }) => project.toLowerCase() === projectCity.toLowerCase()
-        );
-        resolve(obj || res[0]);
-      } else {
-        reject('No local config');
-      }
+    //   if (res.length >= 1) {
+    //     const obj = res.find(
+    //       ({ project }) => project.toLowerCase() === projectCity.toLowerCase()
+    //     );
+    //     resolve(obj || res[0]);
+    //   } else {
+    //     reject('No local config');
+    //   }
   
-    });
+    // });
   }
 };
 
